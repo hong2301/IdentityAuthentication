06a53241d13fdcd34c90d788bea7bece050a9bdc
疯了个疯 <honononong@icloud.com>
2025-07-30 02:52:30 +0800

    refactor(): 前端结构调整

--- frontEnd/src/electron.js
+++ frontEnd/src/electron.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+import { app, BrowserWindow } from 'electron';
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -10,7 +10,7 @@ const createWindow = () => {
     }
   });
 
-  win.loadFile('src/renderer/dist/index.html');
+  win.loadFile('dist/index.html');
 };
 
 
