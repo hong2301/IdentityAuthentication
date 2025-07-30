6777f5a9848fe2d14cf209ecc78b3d3aabdee06d
疯了个疯 <honononong@icloud.com>
2025-07-30 13:53:31 +0800

    feat(): 按钮组件

--- frontEnd/src/components/pushButton.vue
+++ frontEnd/src/components/pushButton.vue
@@ -1,16 +1,61 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 
+// 定义 props
+const props = defineProps({
+  data: {
+    type: Object as () => { title: string; icon: string },
+    required: true,
+    default: () => ({ title: '默认标题', icon: '默认图标' }),
+  },
+  width: {
+    type: [String, Number],
+    default: '100%',
+  },
+  height: {
+    type: [String, Number],
+    default: 'auto',
+  },
+})
+
 onMounted(() => {
-  console.log('按钮')
+  console.log('按钮', props.data.title, props.data.icon)
 })
 </script>
 
 <template>
-  <div class="overture">123</div>
+  <div
+    class="overture"
+    :style="{
+      width: typeof width === 'number' ? `${width}px` : width,
+    }"
+  >
+    <div
+      class="icon"
+      :style="{
+        width: typeof width === 'number' ? `${width}px` : width,
+        height: typeof height === 'number' ? `${height}px` : height,
+      }"
+    ></div>
+    <div class="title">{{ data.title }}</div>
+  </div>
 </template>
 
 <style scoped>
 .overture {
+  cursor: pointer;
+}
+.icon {
+  background-color: aqua;
+  border-radius: 20%;
+  cursor: pointer;
+}
+.title {
+  width: 100%;
+  margin-top: 5%;
+  font-weight: 800;
+  color: white;
+  display: flex;
+  justify-content: center;
 }
 </style>
--- frontEnd/src/views/backHome.vue
+++ frontEnd/src/views/backHome.vue
@@ -1,9 +1,68 @@
 <script setup lang="ts">
 import { useCmdStore } from '@/stores/cmd'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import pushButton from '@/components/pushButton.vue'
 
 const cmdStore = useCmdStore()
+const btns = ref([
+  {
+    title: '体检设置',
+    icon: '12',
+  },
+  {
+    title: '业务记录',
+    icon: '',
+  },
+  {
+    title: '修改密码',
+    icon: '',
+  },
+  {
+    title: '打印机设置',
+    icon: '',
+  },
+  {
+    title: '摄像头设置',
+    icon: '',
+  },
+  {
+    title: '操作时间',
+    icon: '',
+  },
+  {
+    title: '操作流程',
+    icon: '',
+  },
+  {
+    title: '硬件测试',
+    icon: '',
+  },
+  {
+    title: '拖鞋设置',
+    icon: '',
+  },
+  {
+    title: '证件设置',
+    icon: '',
+  },
+  {
+    title: '相机设置',
+    icon: '',
+  },
+  {
+    title: '网络支付',
+    icon: '',
+  },
+  {
+    title: '拍照设置',
+    icon: '',
+  },
+  {
+    title: '准驾车型设置',
+    icon: '',
+  },
+])
+
 onMounted(() => {
   cmdStore.backBtn = 1
   cmdStore.closeBtn = 1
@@ -13,7 +72,11 @@ onMounted(() => {
 
 <template>
   <div class="content">
-    <div class="btn-box"></div>
+    <div class="btn-box">
+      <div v-for="(bItem, bIndex) in btns" :key="bIndex" class="btn-item">
+        <pushButton :data="bItem" :width="120" :height="120" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,7 +84,16 @@ onMounted(() => {
 .content {
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .btn-box {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr); /* 创建5列等宽的网格 */
+  gap: 50px; /* 设置网格项之间的间隙 */
+}
+.btn-item {
+  cursor: pointer;
 }
 </style>
