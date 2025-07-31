7311fc9efa193bcaa401795169703bc3e3f77166
疯了个疯 <honononong@icloud.com>
2025-07-31 15:41:23 +0800

    feat(): 体验下一步选择的标题

--- frontEnd/src/views/process/exam.vue
+++ frontEnd/src/views/process/exam.vue
@@ -12,6 +12,7 @@ onMounted(() => {
 
 <template>
   <div class="content">
+    <div class="title">请选择下一步要进行的操作</div>
     <div class="card-box">
       <div class="card"></div>
       <div class="card"></div>
@@ -28,19 +29,31 @@ onMounted(() => {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+}
+.title {
+  margin-bottom: 2%;
+  font-size: 3rem;
+  color: white;
+  font-weight: 800;
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .card-box {
   width: 80%;
-  height: 100%;
+  height: 50%;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 .card {
   width: 30%;
-  height: 50%;
+  height: 100%;
   background: linear-gradient(145deg, #1a56b4, #0d47a1);
   border-radius: 2%;
+  cursor: pointer;
 }
 .overtime {
   position: fixed;
