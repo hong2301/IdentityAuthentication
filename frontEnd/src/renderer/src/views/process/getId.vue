015f22c9856079cb3a7c38f54648bc7356d0897e
疯了个疯 <honononong@icloud.com>
2025-08-05 11:19:01 +0800

    refactor(): 身份证获取调整

--- frontEnd/src/renderer/src/views/process/getId.vue
+++ frontEnd/src/renderer/src/views/process/getId.vue
@@ -9,6 +9,8 @@ import BtnBox from '@/components/btnBox.vue'
 import { useProjectStore } from '@/stores/project'
 const cmdStore = useCmdStore()
 
+const timeoutBtn = ref(false)
+const overtimeRef = ref()
 const nextPageData = ref({
   path: '/layout',
   seconds: 30000,
@@ -42,6 +44,29 @@ const btns = ref<btnType[]>([
     },
   },
 ])
+const overtimeBtns = ref<btnType[]>([
+  {
+    label: '返回',
+    key: 'back',
+    type: 'primary',
+    icon: markRaw(Back),
+    position: 'left',
+    onClick: () => {
+      router.go(-1)
+    },
+  },
+  {
+    label: '继续',
+    key: 'continue',
+    type: 'success',
+    icon: markRaw(Right),
+    position: 'right',
+    onClick: () => {
+      timeoutBtn.value = false
+      overtimeRef.value.runTime()
+    },
+  },
+])
 
 const backHandleBack = () => {
   router.go(-1)
@@ -66,8 +91,15 @@ onMounted(() => {
   <div class="content">
     <div class="title">请刷二代身份证</div>
     <div class="card"></div>
-    <overtime :time-num="300" :nextPageData="nextPageData" class="overtime" />
     <BtnBox :btns="btns" />
+    <overtime
+      ref="overtimeRef"
+      v-model:timeout-btn="timeoutBtn"
+      :btns="overtimeBtns"
+      :time-num="300"
+      :nextPageData="nextPageData"
+      class="overtime"
+    />
   </div>
 </template>
 
