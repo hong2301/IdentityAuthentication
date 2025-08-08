0f5500fa1ca3d3594dbf2b1e80dcda6c034d7c32
hong <honononong@icloud.com>
2025-08-08 17:26:28 +0800

    feat(): 删除冗余代码

--- frontEnd/forge.config.js
+++ frontEnd/forge.config.js
@@ -4,9 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    files: [
-      "src/renderer/*",
-    ],
+    files: [],
   },
   rebuildConfig: {},
   makers: [
