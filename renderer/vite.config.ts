b9dec1855756f491094c97b7aee7de60bd82bf5e
疯了个疯 <honononong@icloud.com>
2025-08-12 16:03:41 +0800

    refactor(): 前端打包输出调整

--- renderer/vite.config.ts
+++ renderer/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     vueDevTools(),
   ],
   build: {
-    outDir: 'dist', // 输出到上级dist
+    outDir: '../electron/src/renderer', // 输出到上级dist
   },
   resolve: {
     alias: {
